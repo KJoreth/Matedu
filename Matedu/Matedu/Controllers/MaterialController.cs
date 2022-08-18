77f0b99154139f557231a45d95c76a34db1a6eef
KJoreth <kjoreth97@gmail.com>
2022-08-18 20:21:08 +0200

    Implemented material sorting system

--- Matedu/Matedu/Controllers/MaterialController.cs
+++ Matedu/Matedu/Controllers/MaterialController.cs
@@ -9,9 +9,28 @@ namespace Matedu.Controllers
             => _materialServices = materialServices;
 
         [HttpGet]
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(string sortOrder)
         {
+            ViewData["NameSortParm"] = sortOrder == "name" ? "" : "name";
+            ViewData["AuthorSortParm"] = sortOrder == "author" ? "" : "author";
+            ViewData["TypeSortParm"] = sortOrder == "type" ? "" : "type";
             var materials = await _materialServices.GetAllAsync();
+
+            switch (sortOrder)
+            {
+                case "name":
+                    materials = materials.OrderBy(m => m.Title).ToList();
+                    break;
+                case "author":
+                    materials = materials.OrderBy(m => m.Author.Name).ToList();
+                    break;
+                case "type":
+                    materials = materials.OrderBy(m => m.Type.Name).ToList();
+                    break;
+                default:
+                    break;
+            }
+
             return View(materials);
         }
     }
